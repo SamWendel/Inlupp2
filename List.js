0a37939e6be89ee53459d3d5d0c0fba88a135de7
SamWendel <sam.wendel@hotmail.se>
2021-03-18 18:21:08 +0100

    first commit

---
+++ List.js
@@ -0,0 +1,26 @@
+fetchBeer();
+function fetchBeer(){
+    fetch("https://api.punkapi.com/v2/beers?page=2&per_page=42")
+    .then(resp => resp.json())
+    .then((beers) => render(beers));
+}
+
+function render(beers) {
+    let output = "";
+
+    beers.forEach((beers) => { 
+        output += `
+        <div class="card col-sm-4" style="width: 18rem; background-color: #DCDCDC;">
+        <img style="width: 100px; height: auto" src=${beers.image_url} class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${beers.name}</h5>
+          <p class="card-text">${beers.tagline}</p>
+          <p class="card-text">${beers.description}</p>
+        </div>
+      </div>`;
+    });
+
+    document.getElementById("output").innerHTML = output;
+}
+
+
