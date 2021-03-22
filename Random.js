35a3135047acd91eadef8c28a796da55d427b45d
SamWendel <sam.wendel@hotmail.se>
2021-03-22 14:27:45 +0100

    Fourth

--- Random.js
+++ Random.js
@@ -8,10 +8,22 @@ function fetchBeer(){
 function render(beers) {
     let output = "";
 
-    beers.forEach((beers) => { 
+    beers.forEach((beers) => {
+
+        var tempImage;
+        var img = new Image();
+        img.src = "Images/placeholder.png";
+
+        if (beers.image_url === null){
+            tempImage = img.src;
+        }
+        else {
+            tempImage = beers.image_url;
+        }
+         
         output += `
         <div class="div-random-main" style="width: 18rem; background-color: #DCDCDC;">
-        <img style="width: 100px; height: auto" class="img-random" src="${beers.image_url}">
+        <img style="width: 100px; height: auto" class="img-random" src="${tempImage}">
         <h3 class="h3-random-name">${beers.name}</h3>
         <div class="div-random-tagline">${beers.tagline}</div>
         </div>`;
